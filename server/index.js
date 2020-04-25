868b9a5a4217564da02826033ac69afe5049f60b
kaioGama <kaioeduardoescar@gmail.com>
2020-04-25 02:44:41 -0400

    [Socket]  --> Implementation

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const http = require('http')
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
+const router = require('./router');
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connectiom!!!");
+    socket.on('disconnect', () => {
+        console.log("User had left!!!");
+    });
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT} and url http://localhost:${PORT}`));
 
